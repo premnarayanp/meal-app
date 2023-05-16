b752f4c3660870b924d30964781edf2f980382e7
premnarayanp <premnarayanpatel223@gmail.com>
2023-05-16 11:41:21 +0530

    fetch updated and Render meal on home,add and remove Favorites

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,140 @@
 //const url='www.themealdb.com/api/json/v1/1/search.php?f=a'
+const favoritesKey = '_MYFavoritesKey_';
 
-async function getInputForSearch(e) {
-    let target = e.target;
-    let value = target.value;
-    let firstLetter = value.charAt(0);
-    let newSearchURL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`;
-    console.log(newSearchURL);
-    const response = await fetch(newSearchURL);
-    console.log("response", response);
+const mealUlList = document.getElementById('suggested-meal-list');
+const searchInput = document.getElementById('search-input');
+var oldSearch = "";
 
-    const jsonData = await response.json();
-    console.log("json", jsonData);
+// get change in value of input and event with every change in search input
+searchInput.addEventListener('input', (e) => {
+    const target = e.target;
+    const value = target.value;
+    const firstLetter = value.charAt(0);
+    if (!firstLetter || firstLetter == oldSearch) {
+        return;
+    }
+    oldSearch = firstLetter;
+    mealUlList.innerText = ' ';
+    // console.log(firstLetter);
 
-    const data = jsonData.meals;
-    console.log(data);
+    const searchURL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`;
+    searchByFirstLetter(searchURL);
+});
+
+// searchByFirstLetter() search data from meals API ,given first letter
+async function searchByFirstLetter(searchURL) {
+
+    try {
+        const response = await fetch(searchURL);
+        // console.log("json", response);
+        const jsonData = await response.json();
+        // console.log("json", jsonData);
+        const meals = jsonData.meals;
+        // console.log(meals);
+
+        if (meals == null) {
+            const h1 = document.createElement('h1');
+            // h1.innerText = "Related Results Note Found";
+            h1.innerText = `Meals Note Found,which name start with ${value}`;
+            h1.style = "font-size:30px";
+            h1.style = "color:red";
+            mealUlList.appendChild(h1);
+            return;
+        }
+        renderMealListInHome(meals);
+
+    } catch (error) {
+        const h1 = document.createElement('h1');
+        h1.innerText = error;
+        h1.style = "font-size:30px";
+        h1.style = "color:red";
+        mealUlList.appendChild(h1);
+    }
 
 }
 
+//render related Each meal in homepage <ul> list
+function renderMealListInHome(meals) {
+    //let favorite = {};
+    //favorites stored in localStorage  in object in key value pair
+    let favorite = getFavoritesFromLocal(favoritesKey);
 
+    meals.forEach((data) => {
+        let li = document.createElement('li');
+        let span = document.createElement('span');
+        let button = document.createElement('button');
+        const mealId = data.idMeal;
 
+        li.className = "results-item";
+        span.className = "meals-name";
+        span.innerText = data.strMeal;
 
+        //button.className = "favorites-btn";
+        button.id = 'meal' + mealId;
 
+        if (favorite && favorite['meal' + mealId]) {
+            button.innerText = "Remove Fav";
+            button.setAttribute('onclick', `removeFavorites(${mealId})`);
+        } else {
+            button.innerText = "Add   Fav";
+            button.setAttribute('onclick', `addInFavorites(${mealId})`);
+        }
 
+        li.appendChild(span);
+        li.appendChild(button);
+        mealUlList.appendChild(li);
+    });
 
+    //add scrollbar when list is largest
+    if (meals.length > 7) {
+        mealUlList.style = "overflow-y: scroll"
+    } else {
+        mealUlList.style = "overflow-y: hidden"
+    }
+}
 
+//add in my favorites
+function addInFavorites(mealId) {
+    let favoritesList = getFavoritesFromLocal(favoritesKey);
+    let mealKey = 'meal' + mealId;
+    // console.log("before fav", favoritesList);
+    favoritesList[mealKey] = true;
+    // console.log("after fav", favoritesList);
+    storeFavoritesInLocal(favoritesKey, favoritesList);
 
+    favBtn = document.getElementById('meal' + mealId);
+    favBtn.setAttribute('onclick', `removeFavorites(${mealId})`);
+    favBtn.innerText = "Remove Fav";
+}
 
+//remove in my favorites
+function removeFavorites(mealId) {
+    let favoritesList = getFavoritesFromLocal(favoritesKey);
+    let mealKey = 'meal' + mealId;
+    favoritesList[mealKey] = false;
+    delete favoritesList[mealKey];
 
-// (function() {
+    favBtn = document.getElementById('meal' + mealId);
+    favBtn.setAttribute('onclick', `addInFavorites(${mealId})`);
+    favBtn.innerText = "Add   Fav";
 
-// })();
+    storeFavoritesInLocal(favoritesKey, favoritesList);
+}
 
-// const searchInput = document.getElementById('search-input');
-// searchInput.addEventListener('onchange', (e) => {
-//     let target = e.target;
-//     let value = target.value;
-//     console.log(value);
-// });
+// add meals in  favorite,s localStorage in key value pair
+function storeFavoritesInLocal(key, favoritesList) {
+    let favoritesListStr = JSON.stringify(favoritesList);
+    localStorage.setItem(key, favoritesListStr);
+    //console.log("meals Stored successfully");
+}
+
+// get favorites from localStorage
+function getFavoritesFromLocal(key) {
+    let favoritesListStr = localStorage.getItem(key);
+    if (favoritesListStr == null) {
+        return {};
+    }
+    let favoritesList = JSON.parse(favoritesListStr);
+    //console.log("Meals get successfully");
+    return favoritesList;
+}
