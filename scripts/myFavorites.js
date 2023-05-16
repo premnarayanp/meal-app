2967f3eefdbed63ca6c61704b84aa5aff7dc24ca
premnarayanp <premnarayanpatel223@gmail.com>
2023-05-16 19:22:30 +0530

    myFavrites.js  removeFavorites() function adde

--- scripts/myFavorites.js
+++ scripts/myFavorites.js
@@ -52,3 +52,33 @@ function renderMyFavorites() {
 
 }
 renderMyFavorites();
+
+function removeFavorites(mealKey) {
+    let favoritesList = getFavoritesFromLocal(favoritesKey);
+    delete favoritesList[mealKey];
+    console.log(favoritesList);
+    storeFavoritesInLocal(favoritesKey, favoritesList);
+    count -= 1;
+    renderMyFavorites();
+}
+
+
+
+// add meals in  favorite,s localStorage in key value pair
+function storeFavoritesInLocal(key, favoritesList) {
+    let favoritesListStr = JSON.stringify(favoritesList);
+    localStorage.setItem(key, favoritesListStr);
+    // console.log(favoritesList);
+    //console.log("meals Stored successfully");
+}
+
+// get favorites from localStorage
+function getFavoritesFromLocal(key) {
+    let favoritesListStr = localStorage.getItem(key);
+    if (favoritesListStr == null) {
+        return {};
+    }
+    let favoritesList = JSON.parse(favoritesListStr);
+    //console.log("Meals get successfully");
+    return favoritesList;
+}
