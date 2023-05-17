0a300122e5c38733ecc2e165f1da35611cdc6ab7
premnarayanp <premnarayanpatel223@gmail.com>
2023-05-17 15:35:54 +0530

    fetch mealinfoDetail() from API using mealID

--- scripts/mealInfoDetail.js
+++ scripts/mealInfoDetail.js
@@ -1,6 +1,40 @@
 //get mealId from params
+
 let params = new URLSearchParams(location.search);
 //console.log(params.get('data'));
 
 const mealId = params.get('mealId');
-params.get('mealId');
+//params.get('mealId');
+
+//URL with mealId
+const URL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+
+
+let mealName = document.getElementById('meal-name');
+
+//get/fetch detail of meal by mealId
+async function getMealInfoDetail(URL) {
+
+    try {
+        const response = await fetch(URL);
+        // console.log("json", response);
+        const jsonData = await response.json();
+        // console.log("json", jsonData);
+        const mealsDetail = jsonData.meals;
+        console.log(mealsDetail);
+
+        if (mealsDetail == null) {
+            mealName.innerText = `Meals Detail not Found,which ID is ${mealId}`;
+            mealName.style = "font-size:30px";
+            mealName.style = "color:red";
+            return;
+        }
+        // renderMealDetailOnUI(mealsDetail);
+
+    } catch (error) {
+        mealName.innerText = error;
+        mealName.style = "font-size:30px";
+        mealName.style = "color:red";
+    }
+
+}
